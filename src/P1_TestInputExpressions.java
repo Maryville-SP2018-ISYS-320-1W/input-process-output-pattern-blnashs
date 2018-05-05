d441980449a2530fa9c1a1006dc2100aec7ca9ac
Barbara Nash <bnash1@live.maryville.edu>
2018-05-05 13:37:07 -0500

    string 2

--- src/P1_TestInputExpressions.java
+++ src/P1_TestInputExpressions.java
@@ -9,15 +9,15 @@ import java.util.Scanner;
 /*
  Your expected results after the input is provided:
  
- 1.
- 2.
- 3. 
- 4.
+ 1. 34.5
+ 2. 6.0
+ 3. exception
+ 4. exception
  
- 5.
- 6.
- 7.
- 8.
+ 5. exception
+ 6. exception
+ 7. exception
+ 8.  645.0
   
  */
 
@@ -26,7 +26,11 @@ public class P1_TestInputExpressions {
 	public static void main(String[] args) {
 		Scanner console = new Scanner(System.in); 
 		System.out.print("How much money do you have? "); 
-		double money = console.nextDouble(); 
+		double money = console.nextDouble();
+		
+		
+		
+		
 
 	}
 
--- src/P2_Doubler.java
+++ src/P2_Doubler.java
@@ -7,7 +7,18 @@
 public class P2_Doubler {
 
 	public static void main(String[] args) {
-
+// read a phrase from the user
+		Scanner kb = new Scanner(System.in);
+		System.out.print("What is your phrase? ");
+		String phrase = nextLine();
+		// read how many times
+		System.out.print("How many times should I repeat it? ");
+		int numTimes = nextInt ();
+		//newline for formatting
+		System.out.println();
+		//iterate numTimes, printing phrase each time
+		for(int i = 0; i < numTimes; i++)
+			System.out.println(phrase);
 	}
 
 }
